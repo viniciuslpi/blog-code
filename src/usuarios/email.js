a5c6c37fca03eb0ebc19cf0f9ef658272fc2921a
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-08-15 10:38:26 -0300

    add email verification with production infos

--- src/usuarios/email.js
+++ src/usuarios/email.js
@@ -1,28 +1,48 @@
 const nodemailer = require('nodemailer');
 
-class Email {
+const criaConfiguracaoEmailTeste = (contaTeste) => ({
+    host: 'smtp.ethereal.email',
+    auth: contaTeste,
+});
 
-    async enviaEmail() {
+const configuracaoEmailProducao = {
+    host: process.env.EMAIL_HOST,
+    auth: {
+        user: process.env.EMAIL_USUARIO,
+        pass: process.env.EMAIL_SENHA
+    },
+    secure: true
+}
+
+async function criaConfiguracaoEmail() {
+    if (process.env.NODE_ENV === 'production') {
+        return configuracaoEmailProducao;
+    } else {
         const contaTeste = await nodemailer.createTestAccount();
-        const transportador = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            auth: contaTeste,
-        });
-        const info = await transportador.sendMail(this);
-    
-        console.log('URL: '+ nodemailer.getTestMessageUrl(info));
-    
+        return criaConfiguracaoEmailTeste(contaTeste);
     }
 }
 
-class EmailVerificacao extends Email{
-    constructor(usuario, endereco){
+class Email {
+
+    async enviaEmail() {
+        const configuracaoEmail = await criaConfiguracaoEmail();
+        const transportador = nodemailer.createTransport(configuracaoEmail);
+        const info = await transportador.sendMail(this);
+        if (process.env.NODE_ENV !== 'production') {
+            console.log('URL: ' + nodemailer.getTestMessageUrl(info));
+        }
+    }
+}
+
+class EmailVerificacao extends Email {
+    constructor(usuario, endereco) {
         super();
-        this.from = '"Blog do Cógido" <noreply@blogdocodigo.com.br>';
+        this.from = '"Blog do Cógido" <vinicius.pimentel@tems.com.br>';
         this.to = usuario.email;
         this.subject = 'Verificação de email';
         this.text = `Olá! Verifique seu email aqui: ${endereco}`;
-        this.html = `<h1>Olá!</h1> Verifique seu email aqui: <a href="${endereco}">${endereco}</a>`;
+        this.html = `<h1>Olá!</h1> Verifique seu email aqui: <a href="http://${endereco}">${endereco}</a>`;
     }
 }
 
